742758681c7f437fed5960ccc9ae6c6c0785354c
jht043 <jht043@naver.com>
2023-09-05 14:06:31 +0900

    feat : financialProducts 관련 엔티티

---
+++ backend/src/main/java/com/woowahanbank/backend/domain/customer/controller/CustomerController.java
@@ -0,0 +1,7 @@
+package com.woowahanbank.backend.domain.customer.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+}
---
+++ backend/src/main/java/com/woowahanbank/backend/domain/customer/domain/Loaner.java
@@ -0,0 +1,43 @@
+package com.woowahanbank.backend.domain.customer.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.woowahanbank.backend.domain.financialproducts.domain.Loan;
+import com.woowahanbank.backend.domain.user.domain.User;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class Loaner {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	@ManyToOne
+	@JoinColumn(name = "loan_id")
+	private Loan loan;
+	private Boolean grant;
+	private int money;
+	@CreationTimestamp
+	private LocalDateTime date;
+	
+}
