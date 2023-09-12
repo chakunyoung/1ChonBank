ab1564c85397097a62defa60d4babe4272d49e28
박종민 <superfoward@naver.com>
2023-09-12 14:14:08 +0900

    feat: gpt 응답을 문제, 선지, 정답, 해설로 저장

--- backend/src/main/java/com/woowahanbank/backend/domain/quiz/dto/GptResponseDto.java
+++ backend/src/main/java/com/woowahanbank/backend/domain/quiz/dto/GptResponseDto.java
@@ -1,6 +1,9 @@
 package com.woowahanbank.backend.domain.quiz.dto;
 
 import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
 
 @Data
 @NoArgsConstructor
@@ -8,6 +11,37 @@ import lombok.*;
 @Builder
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GptResponseDto {
-	private String content;
+
+	private String id;
+	private String object;
+	private Long created;
+	private String model;
+	private List<Choice> choices;
+
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	@Getter
+	@Setter
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public static class Choice {
+		private int index;
+		private Message message;
+
+	}
+
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@Builder
+	@Getter
+	@Setter
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public static class Message {
+		private String role;
+		private String content;
+	}
+
+
 }
