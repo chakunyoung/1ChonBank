e7cc40abca96732286037d82c006d1e7f8388f3c
jht043 <jht043@naver.com>
2023-09-12 15:46:10 +0900

    fix: 특정 금융 상품 조회 오류 수정

--- backend/src/main/java/com/woowahanbank/backend/domain/financialproducts/repository/FinancialProductRepository.java
+++ backend/src/main/java/com/woowahanbank/backend/domain/financialproducts/repository/FinancialProductRepository.java
@@ -5,10 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.woowahanbank.backend.domain.financialproducts.domain.FinancialProduct;
-import com.woowahanbank.backend.domain.financialproducts.domain.ProductType;
 
 public interface FinancialProductRepository extends JpaRepository<FinancialProduct, Long> {
 	List<FinancialProduct> findAllByFamily_IdOrderByIdDesc(Long familyId);
 
-	List<FinancialProduct> findAllByFamily_IdAndProductTypeOrderByIdDesc(Long familyId, ProductType type);
+	List<FinancialProduct> findAllByFamily_IdAndProductTypeOrderByIdDesc(Long familyId, String productType);
 }
