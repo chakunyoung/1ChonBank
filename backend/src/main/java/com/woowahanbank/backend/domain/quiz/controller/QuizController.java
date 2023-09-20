323eaf8c984ff962e53a61868dfa14e0e706b780
차건영 <chakunyou@naver.com>
2023-09-20 10:13:35 +0900

    feat: DB 퀴즈 풀이 검증

--- backend/src/main/java/com/woowahanbank/backend/domain/quiz/controller/QuizController.java
+++ backend/src/main/java/com/woowahanbank/backend/domain/quiz/controller/QuizController.java
@@ -1,15 +1,20 @@
 package com.woowahanbank.backend.domain.quiz.controller;
 
-		import org.springframework.http.ResponseEntity;
-		import org.springframework.web.bind.annotation.RestController;
-		import org.springframework.beans.factory.annotation.Autowired;
-		import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
-		import com.woowahanbank.backend.domain.quiz.domain.Quiz;
-		import com.woowahanbank.backend.domain.quiz.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-		import java.util.Map;
-		import java.util.Optional;
+import com.woowahanbank.backend.domain.quiz.domain.Quiz;
+import com.woowahanbank.backend.domain.quiz.service.QuizService;
+import com.woowahanbank.backend.domain.user.domain.User;
+import com.woowahanbank.backend.global.auth.security.CustomUserDetails;
 
 @RestController
 @RequestMapping("/api/quiz")
@@ -26,8 +31,9 @@ public class QuizController {
 	public ResponseEntity<Quiz> getQuiz() {
 		Optional<Quiz> todayQuiz = quizService.findTodayQuiz();
 		return todayQuiz.map(quiz -> ResponseEntity.ok().body(quiz))
-				.orElseGet(() -> ResponseEntity.notFound().build());
+			.orElseGet(() -> ResponseEntity.notFound().build());
 	}
+
 	@PostMapping("/make")
 	public ResponseEntity<String> makeQuiz() {
 		quizService.makeQuiz();
@@ -40,5 +46,11 @@ public class QuizController {
 		return ResponseEntity.ok("Quiz를 전부 삭제 했습니다.");
 	}
 
+	@PostMapping("/solved")
+	public ResponseEntity<String> solvedQuiz(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+		User user = customUserDetails.getUser();
+		quizService.solvedQuiz(user);
+		return ResponseEntity.ok("Quiz를 푸셨습니다.");
+	}
 
 }
