789b21d2597d1bc86d244d17198e0aa403995681
차건영 <chakunyou@naver.com>
2023-09-21 11:19:31 +0900

    feat: 미션 다시 수정

--- backend/src/main/java/com/woowahanbank/backend/domain/mission/service/MissionService.java
+++ backend/src/main/java/com/woowahanbank/backend/domain/mission/service/MissionService.java
@@ -1,44 +1,47 @@
 package com.woowahanbank.backend.domain.mission.service;
 
-import com.woowahanbank.backend.domain.mission.domain.Mission;
-import com.woowahanbank.backend.domain.mission.dto.MissionMakeDto;
-import com.woowahanbank.backend.domain.mission.repository.MissionRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.woowahanbank.backend.domain.mission.domain.Mission;
+import com.woowahanbank.backend.domain.mission.dto.MissionMakeDto;
+import com.woowahanbank.backend.domain.mission.repository.MissionRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class MissionService {
 
-    private final MissionRepository missionRepository;
-    public void createMission(MissionMakeDto missionMakeDto) {
-        missionRepository.save(missionMakeDto.MissiontoEntity(missionMakeDto));
-    }
+	private final MissionRepository missionRepository;
 
-    public List<Optional<Mission>> getMissionByFamilyId (Long missionFamilyId) {
-        return missionRepository.findByFamilyId(missionFamilyId);
+	public void createMission(MissionMakeDto missionMakeDto) {
+		missionRepository.save(missionMakeDto.MissiontoEntity(missionMakeDto));
+	}
 
-    }
+	public List<Optional<Mission>> getMissionByFamilyId(Long missionFamilyId) {
+		return missionRepository.findByMissionFamilyId(missionFamilyId);
 
-    public List<Optional<Mission>> getMissionByChildNickName(String nickname) {
-        return missionRepository.findByChildNickname(nickname);
-    }
+	}
 
-    public Mission updateMission(Mission updatedMission) {
-        return missionRepository.save(updatedMission);
-    }
+	public List<Optional<Mission>> getMissionByChildNickName(String nickname) {
+		return missionRepository.findByChildNickname(nickname);
+	}
 
-    public void deleteMissionById(Long missionId) {
-        missionRepository.deleteById(missionId);
-    }
+	public Mission updateMission(Mission updatedMission) {
+		return missionRepository.save(updatedMission);
+	}
 
-    public void deleteMissionsByTerminateDateBefore(Date terminateDate) {
-        missionRepository.deleteByMissionTerminateDate(terminateDate);
-    }
+	public void deleteMissionById(Long missionId) {
+		missionRepository.deleteById(missionId);
+	}
+
+	public void deleteMissionsByTerminateDateBefore(Date terminateDate) {
+		missionRepository.deleteByMissionTerminateDate(terminateDate);
+	}
 
 }
 
