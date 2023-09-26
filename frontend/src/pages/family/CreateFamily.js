bbfba772cd332ca3c0e56a9a8d042b5705e12160
박종민 <superfoward@naver.com>
2023-09-26 16:37:12 +0900

    feat:chatGPT cron

--- frontend/src/pages/family/CreateFamily.js
+++ frontend/src/pages/family/CreateFamily.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setFamilyName } from "redux/Family";
@@ -8,17 +9,19 @@ import Footer from "components/common/Footer";
 import "./CreateFamily.css";
 
 function CreateFamily() {
+  const [inputFamilyName, setInputFamilyName] = useState('');
   const familyName = useSelector((state) => state.family.familyName);
   const role = useSelector((state) => state.auth.user.roles); // 가족 구성원의 역할 정보를 가져옵니다.
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleCreateFamily = (e) => {
-    if (familyName === '') {
+  const handleCreateFamily = () => {
+    if (inputFamilyName === '') {
       alert("한글자 이상 입력하세요.");
       return;
     }
 
+    dispatch(setFamilyName(inputFamilyName));
     // familyName 변수를 이용하여 경로를 구성하여 POST 요청을 보냅니다.
     apis
       .post(`/api/families/${familyName}`)
@@ -36,7 +39,8 @@ function CreateFamily() {
   };
 
   const handleSetFamilyName = (e) => {
-    dispatch(setFamilyName(e.target.value));
+    setInputFamilyName(e.target.value);
+    console.log(inputFamilyName);
   };
 
   return (
@@ -66,13 +70,12 @@ function CreateFamily() {
           <input
             type="text"
             placeholder="가족명 입력란"
-            value={familyName || ""}
+            value={inputFamilyName}
             onChange={handleSetFamilyName}
           />
           <button onClick={handleCreateFamily}>생성</button>
         </>
       )}
-      <Footer />
     </div>
   );
 }
