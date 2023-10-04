5dbebfdbd920f39ffa116f353bffdc5aecc1cd38
jht043 <jht043@naver.com>
2023-10-04 17:52:14 +0900

    design: 금융상품 신청 수락 거절Card디자인

--- frontend/src/components/finance/ApplyCard.js
+++ frontend/src/components/finance/ApplyCard.js
@@ -1,8 +1,27 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import './ApplyCard.css';
 const ApplyCard = (props) => {
-
-    const applyHandler = () => {        
+    console.log(props.proType);
+    const [memo, setMemo] = useState('');
+    const setMemoHandler = (proType) => {
+        switch (proType) {
+            case "DEPOSIT":
+                setMemo("총 납입 금액 : " + props.applys.money + "P");
+                break;
+            case "SAVINGS":
+                setMemo("매월 납입 금액 : " + props.applys.regularMoney + "P");
+                break;
+            case "LOAN":
+                setMemo("대출 신청 금액 : " + props.applys.money + "P");
+                break;
+            default:
+                break;
+        }
+    }
+    useEffect(() => {
+        setMemoHandler(props.proType);
+    }, [props.proType]);
+    const applyHandler = () => {
         props.applyItem(props.applys.id);
     };
     const refuseHandler = () => {
@@ -11,11 +30,16 @@ const ApplyCard = (props) => {
     return (
         <div>
             {props.applys.grant === null ?
-                <div>
-                    <h3>유저 이름 : {props.applys.userNickname}</h3>
-                    <div>
-                        <button onClick={applyHandler}>수락</button>
-                        <button onClick={refuseHandler}>거절</button>
+                <div className='applyRealCard'>
+                    <div className='applyMenuSet'>
+                        <h3>{props.applys.userNickname}</h3>
+                        <div className='applyCardMenu'>
+                            <button className='apply-allowbutton' onClick={applyHandler}>수락</button>
+                            <button className='apply-deletebutton' onClick={refuseHandler}>거절</button>
+                        </div>
+                    </div>
+                    <div className='applyCardInfo'>
+                        {memo}
                     </div>
                 </div> : null}
         </div>
