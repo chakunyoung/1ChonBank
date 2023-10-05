ec50906e055546a650233fffa8152799c0e0080b
jht043 <jht043@naver.com>
2023-10-05 14:37:57 +0900

    feat: 용돈주기 기능 추가

--- frontend/src/pages/family/PinMoneyModal.js
+++ frontend/src/pages/family/PinMoneyModal.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import './PinMoneyModal.css';
 import './myfamily.css';
 import apis from 'services/api/apis';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { DepositMoney } from "redux/Auth";
 const PinMoneyModal = ({ show, onClose, child, children, onDataChange }) => {
     const [inputValue, setInputValue] = useState('');
     const currentDate = new Date();
     const currentDateString = currentDate.toISOString().split('T')[0];
-
+    const user = useSelector((state) => state.auth.user);
+    const dispatch = useDispatch();
     const pinmoneyAssign = async  () => {
         try {
             await apis.post("api/banking/pinmoney", {
@@ -16,6 +18,7 @@ const PinMoneyModal = ({ show, onClose, child, children, onDataChange }) => {
                 receiveTime: currentDateString,
             });
             onDataChange(inputValue);
+            dispatch(DepositMoney(inputValue));
             onClose();
         } catch (error) {
             console.error('용돈 지정에 실패했습니다:', error);
