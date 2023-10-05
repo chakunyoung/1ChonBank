b2f76f364c8d5bdbf39bbf614d964b4c9ccb0325
김동준 <kimdj4e@naver.com>
2023-10-05 15:27:40 +0900

    dddd

--- frontend/src/components/common/Myaccount.js
+++ frontend/src/components/common/Myaccount.js
@@ -3,17 +3,17 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Myaccount.css';
 import { useSelector } from 'react-redux';
 
+
 const Myaccount = () => {
   const money = useSelector((state) => state.auth.user.money)
 
     return (
       <div className='myaccount-container'>
         <div className='myaccount-textarea' >
-            <div className='myaccount-total'>Total</div>
-            <div className='myaccount-date'>Transaction date</div>
+            <div className='myaccount-total'>내 자산</div>
         </div>
         <div className='myaccount-balance'>
-            {money}
+        {money}P
         </div>
         <></>
 
